6b7b6ce49410d02627158df5a6b4dc1926bd59fc
Nosa Agho <nosaoasis@gmail.com>
2022-07-13 21:25:22 +0100

    Database
    Created the database and the tables for the application.

--- server/app.py
+++ server/app.py
@@ -1,6 +1,28 @@
+from email.policy import default
+from turtle import title
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:...777iamMysql@localhost/flask_react'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Articles(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100))
+  body = db.Column(db.Text())
+  date = db.Column(db.DateTime(), default=datetime.datetime.now)
+
+  def __init__(self, title, body):
+    self.title = title
+    self.body = body
+
+
+
 
 @app.route('/')
 def home():
