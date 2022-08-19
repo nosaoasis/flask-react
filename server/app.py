2563ec595b0b233885efc846b83fe9e9458d9dad
Nosa Agho <nosaoasis@gmail.com>
2022-08-19 09:14:19 +0100

    CRUD Functionalities
    Added the CRUD functionalities (create, read, update and delete/destroy) which will be consumed by the frontend client

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
-from email.policy import default
-from turtle import title
-from flask import Flask, jsonify
+from dataclasses import fields
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
@@ -10,6 +10,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:...777iamMysql@localhost/f
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class Articles(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -21,12 +22,64 @@ class Articles(db.Model):
     self.title = title
     self.body = body
 
+class ArticleSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'title', 'body', 'date')
+
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many=True)
 
 
 
 @app.route('/')
 def home():
   return jsonify({"name":"Nosa Agho"})
+
+
+@app.route('/get', methods=["GET"])
+def get_articles():
+  all_articles = Articles.query.all()
+  results = articles_schema.dump(all_articles)
+  return jsonify(results)
+
+
+@app.route('/get/<id>', methods=['GET'])
+def get_article(id):
+  single_article = Articles.query.get(id)
+  return article_schema.jsonify(single_article)
+
+
+@app.route('/add', methods=['POST'])
+def add_article():
+  title = request.json['title']
+  body = request.json['body']
+
+  articles = Articles(title, body)
+  db.session.add((articles))
+  db.session.commit()
+  return article_schema.jsonify(articles)
+
+
+@app.route('/update/<id>', methods=["PATCH"])
+def update_article(id):
+  single_article = Articles.query.get(id)
+  single_article.title = request.json['title'] or single_article.title
+  single_article.body = request.json['body'] or single_article.body
+
+  db.session.commit()
+  return article_schema.jsonify(single_article)
+
+
+@app.route('/delete/<id>', methods=["DELETE"])
+def delete_article(id):
+  single_article = Articles.query.get(id)
+  db.session.delete(single_article)
+
+  db.session.commit()
+  
+  return article_schema.jsonify(single_article)
+
+
 
 if __name__ == "__main__":
   app.run(debug=True)
